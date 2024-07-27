ee26394f25ee870a77f87c5a7d2b0f4266166dab
Andrew Kolarits <amkolarits0222@gmail.com>
2024-07-27 15:49:01 -0400

    aligned svg

--- update_temp_data.js
+++
@@ -1,9 +0,0 @@
-function removeElementsByClass(className) {
-    // Get all elements with the specified class
-    const elements = document.querySelectorAll(`.${className}`);
-    
-    // Iterate over each element and remove it
-    elements.forEach(element => {
-        element.remove();
-    });
-}
